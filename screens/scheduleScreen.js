ef3355682153fd6c458d8b4c0f100d4a018f1894
zaidschouwey98 <zazar981@hotmail.com>
2021-06-22 10:44:44 +0200

    Add list of unconfirmedworkplans to the screen

--- screens/scheduleScreen.js
+++ screens/scheduleScreen.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import Login from "../components/login";
+import { View, Text } from "react-native";
+import { Card } from "react-native-elements";
+import Provider from "../services/data";
+import { ScrollView } from "react-native";
+import styles from "../components/style";
 
 class ScheduleScreen extends Component {
+  state = {
+    unconfirmedWorkPlans: undefined,
+  };
+  constructor(props) {
+    super(props);
+    this.provider = new Provider();
+  }
+  getUnconfirmedWorkPlans() {
+    this.provider.getUnconfirmedWorkPlans().then((workplans) => {
+      this.setState({ unconfirmedWorkPlans: workplans });
+      console.log(this.state.unconfirmedWorkPlans);
+    });
+  }
+  componentDidMount() {
+    this.getUnconfirmedWorkPlans();
+  }
   render() {
-    return <View></View>;
+    return (
+      <ScrollView>
+        <View>
+          <Text style={styles.title}>Horaire à confirmer :</Text>
+        </View>
+        {this.props.sort == "pharma"
+          ? this.state.pharmaChecks.map((item) => (
+              <ReportCard
+                shouldRender={this.getMissingChecks}
+                sort={this.props.sort}
+                item={item}
+              ></ReportCard>
+            ))
+          : null}
+
+        {this.state.unconfirmedWorkPlans
+          ? this.state.unconfirmedWorkPlans.map((item) => (
+              <Card>
+                <Card.Title>
+                  {item.date} = {item.worktime.type}
+                </Card.Title>
+                <Card.Divider />
+              </Card>
+            ))
+          : null}
+      </ScrollView>
+    );
   }
 }
 
